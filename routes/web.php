ce510b4f2645350b80baabc5b1daac7ac8d6076f
Ewoks123 <ridwan120705@gmail.com>
2026-06-09 23:48:05 +0700

    Fix: redirect root URL to admin login page

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin.login');
 });
 
 Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login');
